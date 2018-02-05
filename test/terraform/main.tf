eacc11d0a3e0e75b5bdbbbf2e43fea7af802b228
Giulio Calzolari <giuliocalzo@gmail.com>
2018-02-05 16:57:31 +0100

    fix typo

--- test/terraform/main.tf
+++ test/terraform/main.tf
@@ -59,7 +59,7 @@ resource "aws_instance" "this" {
   ami                    = "${data.aws_ami.amazon_linux.id}"
   instance_type          = "t2.micro"
   subnet_id              = "${element(data.aws_subnet_ids.all.ids, 0)}"
-  user_data =            = "${var.user_data}"
+  user_data              = "${var.user_data}"
   vpc_security_group_ids = ["${module.security_group.this_security_group_id}"]
   iam_instance_profile   = "${aws_iam_instance_profile.ec2_instance_profile.name}"
   tags = "${merge(
